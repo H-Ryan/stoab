2c81d9a4f39d8c487244536c556e3567211f59ee
H-Ryan <talebi.hd@gmail.com>
2015-05-29 11:13:15 +0200

    Editing about us

---
+++ omoss2.php
@@ -0,0 +1,282 @@
+<?php
+
+ini_set("session.use_only_cookies", TRUE);
+ini_set("session.use_trans_sid", FALSE);
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <title>
+        STÖ AB - Sarvari tolkning och översättning / Om oss
+    </title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="description" content=""/>
+    <meta name="keywords" content=""/>
+
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/owl.carousel.css"/>
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+    <script src="js/jquery.equalheights.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <!--[if lt IE 9]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+            <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0"
+                 height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+    <![endif]-->
+
+    <script type="text/javascript" src="js/mosaic.1.0.1.js"></script>
+
+    <script type="text/javascript">
+
+        jQuery(function($){
+            $('.bar2').mosaic({
+                animation	:	'slide'		//fade or slide
+            });
+        });
+
+    </script>
+
+
+    <style type="text/css">
+
+        /*Demo Styles*/
+        #content{ width:845px; margin:20px auto; padding:10px 30px; }
+        .clearfix{ display: block; height: 0; clear: both; visibility: hidden; }
+
+        .details{ margin:15px 20px; }
+        h4{ font:300 16px 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height:120%; letter-spacing:0.15em; color:#fff; }
+        p{ font:200 12px 'Lucida Grande', Tahoma, Verdana, sans-serif; color:#aaa; }
+        a{ text-decoration:none; }
+
+    </style>
+
+</head>
+<body>
+<div class="page">
+    <!--========================================================
+                              HEADER
+    =========================================================-->
+    <?php include("src/partials/shared/header.php"); ?>
+    <!--========================================================
+                              CONTENT
+    =========================================================-->
+    <section>
+        <div class="container">
+            <div class="row wrap_11 wrap_12">
+                <div class="grid_7">
+                    <h2 class="header_2 indent_1">Om oss</h2>
+
+                    <div class="box_4">
+                        <p class="text_3">
+                            <i class="fa fa-chevron-right"></i> STÖ är ett företag vars affärsidé är att skapa
+                            kommunikation mellan människor och kulturer. Genom innovativa kommunikationslösningar
+                            anpassar vi våra tjänster till modern tid. Våra tjänster lämpar sig främst till skolor,
+                            boende, myndigheter och kommuner och tjänsterna kännetecknas av hög kvalitet, stort
+                            engagemang och flexibilitet.
+                        </p>
+                        <br/>
+
+                        <p class="text_3">
+                            <i class="fa fa-chevron-right"></i> Vår vision är att skapa ett ett samhälle där alla kan
+                            kommunicera med varandra.
+                        </p>
+                        <br/>
+
+                        <p class="text_3">
+                            <i class="fa fa-chevron-right"></i> Vi på STÖ levererar kompletta kommunikationslösningar
+                            inom tolkning, översättning, språkstöd, utbildningar samt förmedling av kontaktpersoner.
+                            Våra uppdrag utförs till högsta kvalitet men till konkurrenskraftiga priser. Detta
+                            åstadkommer vi genom en personlig kontakt med våra kunder, noggrann rekrytering och genom
+                            ett effektivt kvalitetssäkringsarbete.
+                        </p>
+                        <br/>
+                        <!--<a href="#" class="btn_2">read more</a>-->
+                    </div>
+                </div>
+                <div class="grid_5">
+                    <div class="img-wrap">
+                        <img data-src="images/index-1_img01.jpg" class="img_1" src="images/preloader.gif"
+                             alt="Image 1"/>
+                        <img data-src="images/index-1_img02.jpg" class="img_1" src="images/preloader.gif"
+                             alt="Image 2"/>
+                    </div>
+                    <div class="img-wrap">
+                        <img data-src="images/index-1_img02-2.jpg" class="img_1" src="images/preloader.gif"
+                             alt="Image 1"/>
+                        <img data-src="images/index-1_img02-1.jpg" class="img_1" src="images/preloader.gif"
+                             alt="Image 2"/>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row wrap_11">
+                <div class="grid_8">
+                    <div class="wrap_10">
+                        <div class="box_6">
+                            <div class="put-left">
+                                <div class="caption">
+                                    <img src="images/index-3_img01.jpg" alt="Image 1"/>
+                                    <p class="text_2 color_1">
+                                        Jobba med oss
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="caption">
+                                <h3 class="text_2 color_2"><a href="jobb.php">Jobba med oss</a></h3>
+
+                                <p>Är du tolk eller vill du bli tolk hos STÖ AB</p>
+
+                                <p class="text_3">
+                                    Vi söker auktoriserade och utbildade tolkar för kontakt- och distanstolkning. Våra
+                                    tolkar anlitas på timbasis vid behov. Arvodet varierar beroende på typen av uppdrag.
+                                </p>
+                                <a class="btn_2" href="jobb.php">Klick här</a>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg_1 wrap_13 wrap_10">
+            <div class="container">
+                <div class="row">
+                    <div class="grid_12">
+                        <h2 class="header_2 indent_2">
+                            Möt vår personal
+                        </h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="grid_6">
+                        <div class="box_4">
+                            <p class="text_3">
+                                På STÖ arbetar vi som ett team för att ni som kund fullt ut ska kunna tillgodogöra all
+                                den kunskap, kompetens och erfarenhet som våra medarbetarna har. Vårt gemensamma mål är
+                                att alltid erbjuda bästa möjliga service för våra kunder. Nedan finner ni
+                                kontaktuppgifter till alla våra medarbetare för att ni på enklaste sätt ska kunna
+                                kontakta den person som ni söker.
+                            </p>
+
+                            <div class="row">
+                                <div class="grid_4">
+                                    <ul class="list_2 text_6">
+                                        <li><a href="#">Nemat Sarvari / Ledamot ,VD</a></li>
+                                        <li><a href="#">Rami Farajallah /  Myndighetsservice</a></li>
+                                    </ul>
+                                </div>
+                                <div class="grid_4">
+                                    <ul class="list_2 text_6">
+                                        <li><a href="#">Mustafa Azad / Rekryterar</a></li>
+                                        <li><a href="#">Marielle Redinge / Marknadsförare Översätt och tolktjänster</a></li>
+                                        <li><a href="#">Hamed Talebi / Systemansvarig-IT</a></li>
+                                        <li><a href="#">Hussam Khadur / Rådgivare</a></li>
+                                        <li><a href="#">Carolina Johnsson/ International försäljare och Marknadsförare</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid_6">
+                        <div class="img-wrap">
+                            <div id="content">
+
+
+                                <!--Bar 2-->
+                                <div class="mosaic-block bar2">
+                                    <div class="mosaic-overlay">
+                                        <div class="details">
+                                            <h4>Nemat Sarvari, VD</h4>
+                                            <a href="mailto:sarvari@sarvari.se"><p>E-post: sarvari[at]sarvari[dot]se</p></a>
+                                            <p>Telefon: 070 7683 325</p>
+                                        </div>
+                                    </div>
+                                    <div class="mosaic-backdrop"><img src="images/ryan.jpg"/></div>
+                                </div>
+
+
+                                <div class="clearfix"></div>
+                            </div>
+                            <img class="img_2" src="images/ryan.jpg" alt="Image 6"/>
+                            <img class="img_2" src="images/index-1_img07.jpg" alt="Image 7"/>
+                            <img class="img_2" src="images/index-1_img08.jpg" alt="Image 8"/>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row wrap_14">
+                <div class="grid_12">
+                    <h2 class="header_1 wrap_8 color_3">
+                        Våra kärnvärden
+                    </h2>
+                </div>
+            </div>
+            <div class="row wrap_15">
+                <div class="grid_4">
+                    <div class="box_5" data-index="1">
+                        <h3 class="text_2 color_2"><a href="#">Kvalitetsarbete</a></h3>
+
+                        <p class="text_3">
+                            Att kunna leverera och utveckla tjänster som uppfyller kundens behov och önskemål är vårt
+                            övergripande mål. Därför genomför vi regelbundna kundundersökningar inom alla våra
+                            verksamhetsområden. Vårt kvalitetsarbete gäller även våra medarbetare. För att kunna
+                            utveckla och leverera de tjänster som våra kunder eftersträvar krävs det att våra
+                            medarbetare har rätt kompetens för sin uppgift och att de trivs på sin arbetsplats.
+                        </p>
+                    </div>
+                </div>
+                <div class="grid_4">
+                    <div class="box_5" data-index="2">
+                        <h3 class="text_2 color_2"><a href="#">Genuin hjälpsamhet</a></h3>
+
+                        <p class="text_3">
+                            Alla våra medarbetare har ett stort engagemang att vilja hjälpa våra kunder att få det som
+                            de vill ha. Vårt kvalitetsarbete och den genuina hjälpsamhet som våra medarbetare har går
+                            hand i hand med varandra. På STÖ har vi en atmosfär där alla våra medarbetare alltid strävar
+                            efter att ge sitt yttersta och det finns en ambition och ödmjukhet som vi gärna förmedlar
+                            till vår omgivning
+                        </p>
+                    </div>
+                </div>
+                <div class="grid_4">
+                    <div class="box_5" data-index="3">
+                        <h3 class="text_2 color_2"><a href="#">Personligt bemötande</a></h3>
+
+                        <p class="text_3">
+                            Vi vill vara en verksamhet där våra kunder känner att de får personlig kontakt. Var och en
+                            av oss som arbetar på STÖ vill vara där för dig som kund, vilket innebär att vi föredrar den
+                            verbala kontakten med våra kunder. Relationen mellan oss och våra kunder baseras på tillit
+                            och engagemang, och vi strävar efter att vara 100% kontaktbar oavsett på vilket sätt
+                            kontakten sker.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include("src/partials/shared/follow-us.html") ?>
+    </section>
+</div>
+<!--========================================================
+                          FOOTER
+=========================================================-->
+<?php include("src/partials/shared/footer.html"); ?>
+<script src="js/script.js"></script>
+</body>
+</html>
