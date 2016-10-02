65b8a97676f25307b3712ef27fdcc7eac64e74d3
Samuil Dragnev <samuil.dragnev@gmail.com>
2016-10-02 11:46:00 +0200

    new favicon

--- public_html/src/email/emailConfig.php
+++ public_html/src/email/emailConfig.php
@@ -4,8 +4,8 @@
  * Date: 28-01-2015
  * Time: 10:09 PM.
  */
-define('EMAIL_HOST', '127.0.0.1'); //The address of the server mail.your-server.de
-define('EMAIL_USER', 'noreply@c4tolk.se'); //The user
+define('EMAIL_HOST', '127.0.0.1'); //The address of the server
+define('EMAIL_USER', 'noreply@your-server.com'); //The user
 define('EMAIL_PASS', 'Test2015'); //The users' password
 define('EMAIL_PORT', 26); //The servers' port 587
 define('EMAIL_ENCRYPT', 'tls'); //The type of encryption "SSL"/"TLS"
